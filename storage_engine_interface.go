dae7a914447b1573648053dafe7d83b2839cf9e9
xchapter7x <xchapter7x@gmail.com>
2014-11-05 17:35:26 -0500

    firming up storageEngine interface
    deciding on some required methods
    for a storageEngine object.
    
    this will likely change, but its
    a start:

--- storage_engine_interface.go
+++ storage_engine_interface.go
@@ -1,4 +1,10 @@
 package toggle
 
 type storageEngine interface {
+	SetFeatureList(featureList map[string]*feature) (err error)
+	RefreshAll() (err error)
+	RefreshOne(featureName string) (err error)
+	ResetOne(featureName string) (err error)
+	Subscribe() (err error)
+	UnSubscribe() (err error)
 }
