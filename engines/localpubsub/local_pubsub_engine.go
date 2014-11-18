1d6778bf3be94a2ea13374305f94dfc51f76487a
xchapter7x <xchapter7x@gmail.com>
2014-11-17 22:56:12 -0600

    some cleanup
    just removing some cruft and layout
    changes....
    
    tests and solid functionality demos
    to come

--- engines/localpubsub/local_pubsub_engine.go
+++ engines/localpubsub/local_pubsub_engine.go
@@ -37,11 +37,7 @@ func pubsubReciever(s *LocalPubSubEngine) {
 		switch n := s.PubSub.Receive().(type) {
 		case redis.Message:
 			fmt.Printf("Message: %s %s\n", n.Channel, n.Data)
-		case redis.Subscription:
-			fmt.Printf("Subscription: %s %s %d\n", n.Kind, n.Channel, n.Count)
-			if n.Count == 0 {
-				return
-			}
+
 		case error:
 			fmt.Printf("error: %v\n", n)
 			return
@@ -58,6 +54,7 @@ func (s *LocalPubSubEngine) StartSubscriptionListener() {
 				select {
 				case <-s.quit:
 					return
+
 				default:
 					pubsubReciever(s)
 				}
